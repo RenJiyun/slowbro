3db2b880374332177e54e11ab62ec6d75441ff71
renjiyun <wozaijixu@msn.cn>
2020-08-10 00:22:05 +0800

    测试web基本功能

--- src/main/java/com/eggip/slowbro/DemoApplication.java
+++ src/main/java/com/eggip/slowbro/DemoApplication.java
@@ -2,12 +2,22 @@ package com.eggip.slowbro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+	@PostMapping("hello")
+	public String sayHello() {
+		return "hello";
+	}
+
 
 }
