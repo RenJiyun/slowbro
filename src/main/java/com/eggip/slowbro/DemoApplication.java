5f9773361056c7b49f52d211054a54e1ca29b63f
renjiyun <wozaijixu@msn.cn>
2020-08-09 23:12:55 +0800

    初始化

---
+++ src/main/java/com/eggip/slowbro/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.eggip.slowbro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
